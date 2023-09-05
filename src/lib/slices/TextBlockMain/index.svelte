4a99243571b4199912fb542a2674d0b69318082d
Siem-P <64197688+Siem-P@users.noreply.github.com>
2023-09-05 13:47:56 +0200

    First good result

---
+++ src/lib/slices/TextBlockMain/index.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {import("@prismicio/client").Content.TextBlockMainSlice} */
+	export let slice;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+		{#each slice.items as item}
+		  <p>{item.title}</p>
+		  {slice.primary.singletitle}
+	
+		{/each} 
+</section>
+
+
+<pre>
+	{JSON.stringify(slice, null, 2)}
+</pre>
